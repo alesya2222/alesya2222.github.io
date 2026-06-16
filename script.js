3ad9b7820f1afb9a18d059a444e8a5e73fbd7e94
alesya2222 <sho.ealesya@gmail.com>
2026-06-16 10:47:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -644,7 +644,7 @@
 
 // ===== Configuration =====
 const CONFIG = {
-    API_BASE_URL: 'https://byte-slim-wheels-occurring.trycloudflare.com',
+    API_BASE_URL: 'https://lancaster-bless-sequence-logic.trycloudflare.com',
     PROFILE_ENDPOINT: '/api/profile'
 };
 
